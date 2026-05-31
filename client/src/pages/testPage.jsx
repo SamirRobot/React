ec51978019c5950279d4a72070b6281db7919f45
Samir <4ramazanzade@gmail.com>
2026-05-31 11:57:28 +0400

    update nav

--- client/src/pages/testPage.jsx
+++ client/src/pages/testPage.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import Nav from "../commponents/Nav.jsx"
-import Hero from "../commponents/Hero.jsx"
-import MiniCard from "../commponents/MiniCard.jsx"
-import BigCard from "../commponents/BigCard.jsx"
-import Down from "../commponents/Down.jsx"
-import Footer from "../commponents/Footer.jsx"
 
 const TestPage = () => {
   return (
      <>
-     
       <Nav/>
-      <Hero/>
-      <MiniCard/>
-      <BigCard/>
-      <Down/>
-      <Footer/>
     </>
   )
 }
