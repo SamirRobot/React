2cdf2cc2700ee245c1235d67d6afbf8cefb4d2f4
Samir <4ramazanzade@gmail.com>
2026-05-31 11:46:46 +0400

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react' 
 import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+import TestPage from './pages/testPage'
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>} /> 
+      <Route path="/" element={<TestPage/>} /> 
     </Routes>
     
   )
