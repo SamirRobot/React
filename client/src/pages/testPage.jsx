75663b5bac429218a82c91f6c87d9bd4ef30d83e
Samir <4ramazanzade@gmail.com>
2026-05-31 11:52:58 +0400

    update

--- client/src/pages/testPage.jsx
+++ client/src/pages/testPage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import React from 'react'
-import Nav from "../commponents/Nav"
-import Hero from "../commponents/Hero"
-import MiniCard from "../commponents/MiniCard"
-import BigCard from "../commponents/BigCard"
-import Down from "../commponents/Down"
-import Footer from "../commponents/Footer"
+import Nav from "../commponents/Nav.jsx"
+import Hero from "../commponents/Hero.jsx"
+import MiniCard from "../commponents/MiniCard.jsx"
+import BigCard from "../commponents/BigCard.jsx"
+import Down from "../commponents/Down.jsx"
+import Footer from "../commponents/Footer.jsx"
 
 const TestPage = () => {
   return (
